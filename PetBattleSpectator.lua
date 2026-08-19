7d74d727c89fe586360fa959690d1dcba5e8bb92
MartineBoulanger <martine.boulanger@gmail.com>
2026-08-19 10:04:53 +0200

    loading message changed

--- PetBattleSpectator.lua
+++ PetBattleSpectator.lua
@@ -246,7 +246,7 @@ local function OnEvent(self, event, ...)
     UpdateAppearance()
   elseif event == "PLAYER_LOGIN" then
     print(
-      "|cff3FC7EB[Pet Battle Spectator]|r: addon loaded. Open options with: |cffFFFF00/pbs|r or |cffFFFF00/pbspec|r or |cffFFFF00/petbattlespectator|r")
+      "|cff3FC7EB[PBSpectator]|r: v2.0.6 Loaded - open with |cffFFFF00/pbs|r, |cffFFFF00/pbspec|r or |cffFFFF00/petbattlespectator|r")
     if addon.Timer then
       addon.Timer:Initialize()
     end
